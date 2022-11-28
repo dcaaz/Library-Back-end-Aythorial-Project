60d76d8f10abd2b2010ae5449991593def795cf5
isabellexvr <isabellexavier59@gmail.com>
2022-11-28 14:18:35 -0300

    fix: product model price data type

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -4,6 +4,6 @@ export const productSchema = joi.object({
     title: joi.string().required(),
     description: joi.string().required(),
     imageURL: joi.string().uri().required(),
-    price: joi.string().required(),
+    price: joi.number().required(),
     category: joi.string().required().valid("fantasia", "sci-fi", "biografia", "poesia", "romance", "drama", "ficção", "aventura", "HQ", "infantil", "nacional", "terror", "jovem", "erótico", "comédia", "distopia", "utopia", "diário", "educação", "autoajuda")
 });
